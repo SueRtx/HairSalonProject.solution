2fea99ac29548b4b8917a487ae78b0fd01231301
Sue Roberts <suerobertstx@yahoo.com>
2022-07-28 14:20:51 -0500

    Add Controllers

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -4,68 +4,68 @@ using HairSalon.Models;
 using System.Collections.Generic;
 using System.Linq;
 
-// namespace ToDoList.Controllers
-// {
-//   public class CategoriesController : Controller
-//   {
-//     private readonly ToDoListContext _db;
+namespace HairSalon.Controllers
+{
+  public class StylistsController : Controller
+  {
+    private readonly HairSalonContext _db;
 
-//     public CategoriesController(ToDoListContext db)
-//     {
-//       _db = db;
-//     }
+    public StylistsController(HairSalonContext db)
+    {
+      _db = db;
+    }
 
-//     public ActionResult Index()
-//     {
-//       List<Category> model = _db.Categories.ToList();
-//       return View(model);
-//     }
+    public ActionResult Index()
+    {
+      List<Stylist> model = _db.Stylists.ToList();
+      return View(model);
+    }
 
-//     public ActionResult Create()
-//     {
-//       return View();
-//     }
+    public ActionResult Create()
+    {
+      return View();
+    }
 
-//     [HttpPost]
-//     public ActionResult Create(Category category)
-//     {
-//       _db.Categories.Add(category);
-//       _db.SaveChanges();
-//       return RedirectToAction("Index");
-//     }
+    [HttpPost]
+    public ActionResult Create(Stylist stylist)
+    {
+      _db.Stylists.Add(stylist);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
-//     public ActionResult Details(int id)
-//     {
-//       Category thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
-//       return View(thisCategory);
-//     }
-//     public ActionResult Edit(int id)
-//     {
-//       var thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
-//       return View(thisCategory);
-//     }
+    public ActionResult Details(int id)
+    {
+      Stylist thisStylist= _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+      return View(thisStylist);
+    }
+    public ActionResult Edit(int id)
+    {
+      var thisStylist= _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+      return View(thisStylist);
+    }
 
-//     [HttpPost]
-//     public ActionResult Edit(Category category)
-//     {
-//       _db.Entry(category).State = EntityState.Modified;
-//       _db.SaveChanges();
-//       return RedirectToAction("Index");
-//     }
+    [HttpPost]
+    public ActionResult Edit(Stylist stylist)
+    {
+      _db.Entry(stylist).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
-//     public ActionResult Delete(int id)
-//     {
-//       var thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
-//       return View(thisCategory);
-//     }
+    public ActionResult Delete(int id)
+    {
+      var thisStylist= _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+      return View(thisStylist);
+    }
 
-//     [HttpPost, ActionName("Delete")]
-//     public ActionResult DeleteConfirmed(int id)
-//     {
-//       var thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
-//       _db.Categories.Remove(thisCategory);
-//       _db.SaveChanges();
-//       return RedirectToAction("Index");
-//     }
-//   }
-// }
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisStylist= _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+      _db.Stylists.Remove(thisStylist);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
